b407bf0061872e807e5f4971b0f2bb3068210fcf
bradnielsen2981 <brad_nielsen2981@hotmail.com>
2021-05-11 11:15:36 +1000

    latest version

---
+++ myscripts.js
@@ -0,0 +1,28 @@
+alert("loaded");
+
+/* This helper function sends and receive data to and from a server so to avoid a page refresh
+urlstring, methodstring="GET"or"POST", parameterobject={var1:value,var1:value}, crossdomain=true or false, responsehandler=functionname */
+function AJAXrequest(urlstring, methodstring, responsehandler=null, parametersobject=null, sendtype="json", crossdomainbool=true )
+{
+    $(document).ready(function() { //make sure script is fully loaded, otherwise errors will occur
+        $.ajax({
+            type: methodstring,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            crossDomain: crossdomainbool, //THIS IS REQUIRED IF COMMUNICATING TO NON-LOCAL SERVER
+            url: urlstring,
+            data: parametersobject, //{var1:1,var2:"hello"} //like a Python Dictionary
+            dataType : sendtype,  //calls json.parse to convert results (JSON) to Javascript Object
+            success: function(result) {
+                responsehandler(result); //send results to responsehandler function
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+}
+
+function defaulthandler(results)
+{
+    console.log(results.message);
+}
